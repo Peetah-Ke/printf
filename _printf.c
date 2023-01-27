d46bbc683fa897619d9aaa477c543500b9412d97
Peetah <118156324+Peetah-Ke@users.noreply.github.com>
2023-01-27 17:55:15 +0300

    Update _printf.c
    Updated _printf.c

--- _printf.c
+++ _printf.c
@@ -16,6 +16,7 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
@@ -37,15 +38,16 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-			flags, width, precision, size);
-
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
 	print_buffer(buffer, &buff_ind);
+
 	va_end(list);
+
 	return (printed_chars);
 }
 
