6e05011ba5cb76bb17ea13912a84de3c075f1ea3
Peetah <118156324+Peetah-Ke@users.noreply.github.com>
2023-01-27 18:01:29 +0300

    Update utils.c
    updated utils.c

--- utils.c
+++ utils.c
@@ -28,10 +28,13 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
+
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
+
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
+
 	return (3);
 }
 
